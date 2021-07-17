e72d1907001b84ec834a7e8ec9bb908d8c813379
arlaine4 <arthur.laine99@gmail.com>
2021-07-17 14:17:20 +0200

    SAT exercice should be okay now, still need to do ex06 and ex10

--- ex07/sat.py
+++ ex07/sat.py
@@ -1,19 +1,15 @@
+from copy import deepcopy
+
 operators = ['^', '|', '=', '&', '>', '!']
 
-def get_elem_from_rpn(rpn):
-    elems = []
-    for elem in rpn:
-        if elem not in operators:
-            elems.append(elem)
-    return elems
 
 def product(*args, repeat=1):
-    pools = [tuple(pool) for pool in *args] * repeat
+    pools = [tuple(pool) for pool in args] * repeat
     result = [[]]
     for pool in pools:
         result = [x+[y] for x in result for y in pool]
     for prod in result:
-        yield tuple(result)
+        yield tuple(prod)
     return result
 
 def update_stack(stack, elem):
@@ -35,7 +31,7 @@ def update_stack(stack, elem):
             res = 1
     elif elem == '!':
         res = stack.pop()
-        res = 0 if res = 1 else 1
+        res = 0 if res == 1 else 1
     stack.append(res)
     return stack
 
@@ -48,19 +44,48 @@ def parse_rpn(rpn):
             stack = update_stack(stack, elem)
     return stack[0]
 
-def replace_eq_elems_with_combi(rpn, tmp):
-    i_elem = 0
-    rpn = [elem for elem in rpn]
-    for i_rpn, elem in enumerate(rpn):
-        if elem.isalpha():
-            rpn[i_rpn] = elems[i_elem]
-            i_elem += 1
-    return rpn
 
+def build_dico_from_rpn(rpn):
+    dico = {}
+    for index, elem in enumerate(rpn):
+        if elem not in operators:
+            if elem not in dico:
+                dico[elem] = [index]
+            elif elem in dico:
+                dico[elem].append(index)
+    return dico
+
+
+def build_list_order_from_dico(dico):
+    lst_order = []
+    keys = list(dico.keys())
+    lst_order = [elem for elem in keys]
+    return lst_order
+
+
+def replace_eq_elems_with_combi(dico, next_combi, rpn, order):
+    new_rpn = deepcopy(rpn)
+    for i, elem in enumerate(rpn):
+        if elem not in operators:
+            for index in dico[elem]:
+                new_rpn[index] = next_combi[order.index(elem)]
+    return new_rpn
+
+    
 def sat(rpn):
-    elems = get_elems_from_rpn(rpn)
-    combi = product(rpn, len(elems))
+    dico = build_dico_from_rpn(rpn)
+    order = build_list_order_from_dico(dico)
+    combi = product(range(2), repeat=len(dico))
+    rpn = [elem for elem in rpn]
+    sat_eq = False
     while 1:
         try:
-            tmp = next(combi)
-            eq_replaced = replace_eq_elems_with_combi(rpn, tmp)
+            next_combi = next(combi)
+            new_eq = replace_eq_elems_with_combi(dico, next_combi, rpn, order)
+            res = parse_rpn(new_eq)
+            print(res)
+            sat_eq = True if res == 1 else False
+        except:
+            break
+    print("#", sat_eq)
+
